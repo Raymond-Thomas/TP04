cf11e3b47b7ae6f457cc650ae12ebb351c6ee56b
SylvaBHD2 <93947829+SylvaBHD2@users.noreply.github.com>
2022-05-17 09:31:19 +0200

    rename+changes

--- DataTypes/DoublyLinkedList.c
+++ DataTypes/DoublyLinkedList.c
@@ -2,44 +2,170 @@
 // Created by Buhard on 16/05/2022.
 //
 
-#ifndef TP04_DOUBLYLINKEDLIST_H_DANS_DEV_LOT2_H
-#define TP04_DOUBLYLINKEDLIST_H_DANS_DEV_LOT2_H
+#include "DoublyLinkedList.h dans dev_lot2.h"
+#include <stdlib.h>
+#include <string.h>
+#include "DoublyLinkedList.h"
 
-#endif //TP04_DOUBLYLINKEDLIST_H_DANS_DEV_LOT2_H
-//
-// Created by Buhard on 16/05/2022.
-//
+#define START_ASCII_DIGIT 48
+#define END_ASCII_DIGIT 58
+#define MAX_DIGIT_STRING_SIZE 512
 
-#ifndef TP04_DOUBLYLINKEDLIST_H_DANS_DEV_LOT2_H
-#define TP04_DOUBLYLINKEDLIST_H_DANS_DEV_LOT2_H
+static Node* createNode (const Digit digit) {
+    Node* node = malloc (sizeof (Node));
+    if (node != NULL) {
+        node->digit = digit;
+        node->next = NULL;
+        node->previous = NULL;
+    }
+    return node;
+};
 
-#endif //TP04_DOUBLYLINKEDLIST_H_DANS_DEV_LOT2_H
-#ifndef DOUBLY_LINKED_LIST
-#define DOUBLY_LINKED_LIST
+static int addDigit (DoublyLinkedList* doublyLinkedList, const Digit digit, const unsigned short startOrEnd) {
+    int executionSucceed = 0;
+    Node* node = createNode (digit);
 
-typedef unsigned short Digit;
+    if (node == NULL) {
+        executionSucceed = 0;
+    } else if (isNodeEmpty (doublyLinkedList->start)) {
+        doublyLinkedList->start = node;
+        doublyLinkedList->end = node;
+        executionSucceed = 1;
+    } else {
+        if (startOrEnd == 0) {
+            doublyLinkedList->start->previous = node;
+            node->next = doublyLinkedList->start;
+            doublyLinkedList->start = node;
+        } else {
+            doublyLinkedList->end->next = node;
+            node->previous = doublyLinkedList->end;
+            doublyLinkedList->end = node;
+        }
+        executionSucceed = 1;
+    }
+    return executionSucceed;
+}
 
-typedef struct Node {
-    Digit digit;
-    struct Node* next;
-    struct Node* previous;
-} Node;
+static Digit charToDigit (const char digitChar) {
+    if ((digitChar < START_ASCII_DIGIT) || (digitChar > END_ASCII_DIGIT)) {
+        return 0;
+    } else {
+        return digitChar - START_ASCII_DIGIT;
+    }
+}
 
-typedef struct DoublyLinkedList {
-    Node* start;
-    Node* end;
-} DoublyLinkedList;
+int addDigitAtStart (DoublyLinkedList* doublyLinkedList, const Digit digit) {
+    return addDigit (doublyLinkedList, digit, 0);
+}
 
-extern int               addDigitAtStart                  (DoublyLinkedList* doublyLinkedList, const Digit digit);
-extern int               addDigitAtEnd                    (DoublyLinkedList* doublyLinkedList, const Digit digit);
-extern int               isNodeEmpty                      (const Node* node);
-extern DoublyLinkedList* createDoublyLinkedList           (void);
-extern DoublyLinkedList* createDoublyLinkedListFromString (const char* digitString);
-extern char              digitToChar                      (const Digit digit);
-extern void              deleteDoublyLinkedList           (DoublyLinkedList* doublyLinkedList);
-extern int               getDoublyLinkedListLength        (const DoublyLinkedList* doublyLinkedList);
-extern void              removeStartFromDoublyLinkedList  (DoublyLinkedList* doublyLinkedList);
-extern void              removeEndFromDoublyLinkedList    (DoublyLinkedList* doublyLinkedList);
-extern Digit             getDigitByPlaceFromStart         (const DoublyLinkedList* doublyLinkedList, const unsigned int place);
+int addDigitAtEnd (DoublyLinkedList* doublyLinkedList, const Digit digit) {
+    return addDigit (doublyLinkedList, digit, 1);
+}
 
-#endif // !DOUBLY_LINKED_LIST
+static void deleteNode (Node* node) {
+    if (!isNodeEmpty (node->previous)) {
+        node->previous->next = node->next;
+    }
+    if (!isNodeEmpty (node->next)) {
+        node->next->previous = node->previous;
+    }
+    free (node);
+}
+
+int isNodeEmpty (const Node* node) {
+    return node == NULL;
+}
+
+DoublyLinkedList* createDoublyLinkedList (void) {
+    DoublyLinkedList* doublyLinkedList = malloc (sizeof (DoublyLinkedList));
+    if (doublyLinkedList != NULL) {
+        doublyLinkedList->start = NULL;
+        doublyLinkedList->end = NULL;
+    }
+    return doublyLinkedList;
+}
+
+DoublyLinkedList* createDoublyLinkedListFromString (const char* digitString) {
+    DoublyLinkedList* doublyLinkedList = createDoublyLinkedList ();
+
+    if (doublyLinkedList != NULL) {
+        for (unsigned int i = 0; i < strnlen (digitString, MAX_DIGIT_STRING_SIZE); i++) {
+            addDigitAtEnd (doublyLinkedList, charToDigit (*(digitString + i)));
+        }
+    }
+
+    return doublyLinkedList;
+}
+
+char digitToChar (const Digit digit) {
+    if (digit > 9) {
+        return 'E';
+    } else {
+        return digit + START_ASCII_DIGIT;
+    }
+}
+
+void deleteDoublyLinkedList (DoublyLinkedList* doublyLinkedList) {
+    if (doublyLinkedList != NULL) {
+        while (doublyLinkedList->start != NULL) {
+            Node* node;
+            node = doublyLinkedList->start;
+            doublyLinkedList->start = doublyLinkedList->start->next;
+            deleteNode (node);
+        }
+        free (doublyLinkedList);
+    }
+}
+
+int getDoublyLinkedListLength (const DoublyLinkedList* doublyLinkedList) {
+    Node* node;
+    int doublyLinkedListLength = 0;
+    if (doublyLinkedList != NULL) {
+        node = doublyLinkedList->start;
+        while (!isNodeEmpty (node)) {
+            doublyLinkedListLength++;
+            node = node->next;
+        }
+    }
+    return doublyLinkedListLength;
+}
+
+void removeStartFromDoublyLinkedList (DoublyLinkedList* doublyLinkedList) {
+    if (doublyLinkedList->start != NULL) {
+        Node* node;
+        node = doublyLinkedList->start;
+        doublyLinkedList->start = doublyLinkedList->start->next;
+        if (node == doublyLinkedList->end) {
+            doublyLinkedList->end = NULL;
+        }
+        deleteNode (node);
+    }
+}
+
+void removeEndFromDoublyLinkedList (DoublyLinkedList* doublyLinkedList) {
+    if (doublyLinkedList->end != NULL) {
+        Node* node;
+        node = doublyLinkedList->end;
+        doublyLinkedList->end = doublyLinkedList->end->previous;
+        if (node == doublyLinkedList->start) {
+            doublyLinkedList->start = NULL;
+        }
+        deleteNode (node);
+    }
+}
+
+Digit getDigitByPlaceFromStart (const DoublyLinkedList* doublyLinkedList, unsigned int place) {
+    Digit result = 'E';
+
+    if (doublyLinkedList->start != NULL) {
+        Node* node;
+        node = doublyLinkedList->start;
+        for (unsigned int i = 0; (i < place) && !isNodeEmpty (node); i++) {
+            node = node->next;
+        }
+        if (!isNodeEmpty (node)) {
+            result = node->digit;
+        }
+    }
+    return result;
+}
