a9e168c768b864121398a3cbcf30b4d8f9752f2a
SylvaBHD2 <93947829+SylvaBHD2@users.noreply.github.com>
2022-05-24 18:53:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main (void) {
     printHugeUnsignedInt (hugeNumber);
     
     //avec les floats
-    HugeUnsignedFloat* hugeNumber = createHugeUnsignedFloatFromString ("123456789012345678901234567890.56");
+    HugeUnsignedFloat* hugeNumberFloat = createHugeUnsignedFloatFromString ("123456789012345678901234567890.56");
     printHugeUnsignedFloat (hugeNumber);
-    hugeNumber = createHugeUnsignedFloatFromString ("0000000123456789012345678901234567890.452");
+    hugeNumberFloat = createHugeUnsignedFloatFromString ("0000000123456789012345678901234567890.452");
     printHugeUnsignedFloat (hugeNumber);
 }
